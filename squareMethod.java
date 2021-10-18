532ffc603f3a9f8420ab27b7190ca6ba27bedc04
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-18 16:53:37 +0530

    square of number using method call

---
+++ squareMethod.java
@@ -0,0 +1,11 @@
+class Demo{
+    public static void main(String args[]){
+        int result=Demo.Method(5);
+        System.out.print(result);
+    }
+    static int Method(int num)
+        {
+            return num*num;
+        }
+    
+}
