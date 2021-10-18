56fbf80a498a545d2c010fc59a47f77436ae4f5a
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-18 16:46:42 +0530

    break statement in java

---
+++ breakStatement.java
@@ -0,0 +1,13 @@
+class BreakStatement{
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        for(int i:arr)
+        {
+            {
+            if(i==9) //if we write break on that loop terminate 
+          break;     // if write continue i==9 number is left and all print num. 
+        }
+        System.out.println(i);
+        }
+    }
+}
