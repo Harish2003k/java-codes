3d06425674a0d5a7907047969d5694017b6d081b
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-18 16:13:30 +0530

    Unary operator in java

---
+++ UnaryOperator.java
@@ -0,0 +1,11 @@
+class Unaryoperator{
+    public static void main(String args[] ){
+        int x=10;
+        int y=5;
+        System.out.println("Unary Minus Operator:"+(-x));
+         System.out.println("Pre Increment:"+(++x));
+          System.out.println("Post Increment:"+(x++));
+           System.out.println("Pre Decrement:"+(--y));
+            System.out.println("Post Decrement:"+(y--));
+    }
+}
