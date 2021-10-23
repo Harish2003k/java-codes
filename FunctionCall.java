dcff1902d69ed66d2e7118fb3cc73b3f203c944d
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-23 20:32:06 +0530

    function call in java

---
+++ FunctionCall.java
@@ -0,0 +1,21 @@
+class FunctionCall
+{
+    static void fun1()
+    {
+        System.out.println("PYTHON");
+        fun2();
+        System.out.println("C");
+    }
+    static void fun2()
+    {
+        System.out.println("C++");
+    }
+    public static void main(String args[])
+    {
+          System.out.println("Java");
+           fun1();
+         System.out.println("HTML");
+      
+       
+    }
+}
