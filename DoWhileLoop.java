9f5f4cc697f29c050ae35c57867350e4c0076751
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-23 11:32:25 +0530

    infinitive do while loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+class InfinitiveDoWhile
+{
+    public static void main(String args[])
+    {
+        do{
+            System.out.println("Infinitive do while loop:");
+        }
+        while(false);
+    }
+}
