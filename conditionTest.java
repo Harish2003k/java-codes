b61dbd9b3a6b629415089b118095cd19d69b5a26
harish2003k <harishkumargupta2003@bbdu.ac.in>
2021-10-24 23:10:20 +0530

    condition test in programe using turnary operator

---
+++ conditionTest.java
@@ -0,0 +1,8 @@
+class conditionTest {
+    public static void main(String args[]) {
+        String status1;
+        int rank = 3;
+        status = (rank == 1) ? "Done" : "Pending";
+        System.out.println(status1);
+    }
+}
