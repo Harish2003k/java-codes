043a936724065e131522209be855f14c73de8254
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2021-11-06 19:43:43 +0530

    Add files via upload
    Print String and char

---
+++ StringandChar.java
@@ -0,0 +1,11 @@
+class StringDemo{
+    public static void main(String args[]){
+        String str1="Hello";
+        char arr[]={'j','a','v','a'};
+        String str2=new String("String Demo");
+        String str3=new String (arr);
+        System.out.println("String 1:"+str1);
+        System.out.println("String 2:"+str2);
+        System.out.println("String 3:"+str3);
+    }
+}
